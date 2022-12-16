e504f8e71aa98c0889e81bdfa9e83881df7fbb73
zephyr <s-zephyr@o2.pl>
2022-12-16 14:53:08 +0100

    test scroll

--- Assets/Logic/Home.cs
+++ Assets/Logic/Home.cs
@@ -1,5 +1,7 @@
 ﻿
+using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
 namespace Assets.Logic
 {
@@ -7,15 +9,22 @@ namespace Assets.Logic
     {
         public string Name => "Base";
         public List<IActionDescription> Actions { get; private set; } = new List<IActionDescription>();
-
+        private int actionsAdded;
         public Home(BoardState boardState) {
             Actions.Add(new SimpleAction { Name = "Upgrade gatherers", Execute = () => Gatherers += 10 });
             Actions.Add(new SimpleAction { Name = "Upgrade walls", Execute = () => {
-                foreach (var t in boardState.towers) t.Upgrade();
-            }
+                    foreach (var t in boardState.towers) t.Upgrade();
+                }
             });
+            Actions.Add(new SimpleAction { Name = "Add Action", Execute = () => { AddAction(); } }) ;
+
         }
 
+        private void AddAction()
+        {
+            ++actionsAdded;
+            Actions.Add(new SimpleAction { Name = "Add Action "+ actionsAdded, Execute = () => { AddAction(); } });
+        }
 
         public int Hp = 500;
         public int HpMax = 500;
